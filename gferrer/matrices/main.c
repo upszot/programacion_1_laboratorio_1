3cc769713603769cb122c1973475fd2db65914d9
upszot <upszot@gmail.com>
2018-04-13 19:46:44 -0300

    Add files via upload

--- gferrer/matrices/main.c
+++ gferrer/matrices/main.c
@@ -3,13 +3,17 @@
 #include <string.h>
 
 #define TAB 9
-#define TAM 3
+#define TAM 2
 
 
 float calculapromedio(int nota,int nota2);
 void ordenaVectores(int legajo[],int nota1[],int nota2[],float promedio[], char nombre[][50]);
 void mostrar_resultado(int legajo[],int nota1[],int nota2[],float promedio[], char nombre[][50]);
 void cargaVectores(int legajo[],int nota1[],int nota2[],float promedio[], char nombre[][50]);
+void CargaAlumno(int legajo[],int nota1[],int nota2[],float promedio[], char nombre[][50], int index);
+
+void menu();
+int BuscarLibre(int [],int);
 
 int main()
 {
@@ -18,11 +22,67 @@ int main()
     int nota2[TAM];
     float promedio[TAM];
     char nombre[TAM][50];
+    int index;
+    char seguir='s';
+    int opcion;
 
-    cargaVectores(legajo,nota1,nota2,promedio,nombre);
-    ordenaVectores(legajo,nota1,nota2,promedio,nombre);
-    mostrar_resultado(legajo,nota1,nota2,promedio,nombre);
+    do
+    {
+        system("cls");
+        menu();
+        scanf("%d",&opcion);
+        switch(opcion)
+        {
+            case 1: //cargar
+                index=BuscarLibre(legajo,TAM);
+                CargaAlumno(legajo,nota1,nota2,promedio, nombre, index);
+                //llamo a funcion cargar
+                break;
+            case 2: //Mostrar
+                mostrar_resultado(legajo,nota1,nota2,promedio,nombre);
+                system("pause");
+                break;
+            case 3: //mostrar
+                break;
+            case 4: //ordenar
+                break;
+            case 5: //ordenar
+                ordenaVectores(legajo,nota1,nota2,promedio,nombre);
+                break;
+            case 6: //ordenar
+                seguir = 'n';
+                break;
+        }
+    }while(seguir=='s');
+
+    //cargaVectores(legajo,nota1,nota2,promedio,nombre);
+
+
     return 0;
+}
+
+void menu()
+{
+    printf("\n\n 1 Carga: ");
+    printf("\n 2 Mostrar: ");
+    printf("\n 3 Modificar: ");
+    printf("\n 4 Baja: ");
+    printf("\n 5 Ordenar: ");
+    printf("\n 6 Salir (s): ");
+    printf("\n\nElija opcion: ");
+}
+
+int BuscarLibre(int legajo[],int cantidad)
+{
+    int index=-1;
+    for(int i=0;i<cantidad;i++)
+    {
+        if(legajo[i]==0)
+        {
+            index=i;
+        }
+    }
+    return index;
 }
 
 float calculapromedio(int nota,int nota2)
@@ -30,6 +90,45 @@ float calculapromedio(int nota,int nota2)
     float rta;
     rta= ((float)nota+nota2)/2;
     return rta;
+}
+
+void CargaAlumno(int legajo[],int nota1[],int nota2[],float promedio[], char nombre[][50], int index)
+{
+    do
+    {
+        printf("Ingrese el legajo del alumno: ");
+        scanf("%d", &legajo[index]);
+        if(legajo[index] < 1)
+        {
+            printf("El numero de legajo debe ser positivo\n");
+        }
+        } while(legajo[index] < 1);
+
+        fflush(stdin);
+        printf("Ingrese el nombre del alumno: ");
+        gets(nombre[index]);
+
+        do
+        {
+            printf("Ingrese la primer nota del alumno: ");
+            scanf("%d", &nota1[index]);
+            if(nota1[index] < 1 || nota1[index] > 10)
+            {
+                printf("La nota debe estar comprendida entre 1 y 10\n");
+            }
+        } while(nota1[index] < 1 || nota1[index] > 10);
+
+        do
+        {
+            printf("Ingrese la segunda nota del alumno: ");
+            scanf("%d", &nota2[index]);
+            if(nota2[index] < 1 || nota2[index] > 10)
+            {
+                printf("La nota debe estar comprendida entre 1 y 10\n");
+            }
+        } while(nota2[index] < 1 || nota2[index] > 10);
+
+        promedio[index]=calculapromedio(nota1[index],nota2[index]);
 }
 
 void cargaVectores(int legajo[],int nota1[],int nota2[],float promedio[], char nombre[][50])
